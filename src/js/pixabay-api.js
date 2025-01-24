a445d924a22a386311452182147d4b328d3d44c4
TarasRogiv <r.taras.t.1@gmail.com>
2025-01-24 12:41:40 +0200

    upd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,30 +2,21 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-export default async function getResponseData(requestWords, additionalParams) {
-  // Об'єкт параметрів для axios
+export default async function getResponseData(
+  requestWords,
+  additionalParams = {}
+) {
   const requestParams = {
     key: '48329924-6906af0078b1de986ec16b549',
     q: requestWords,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    ...additionalParams,
   };
 
-  // Динамічно додаю нові параметри, якщо вони були передані у функцію у параметрі additionalParams
-  if (Object.keys(additionalParams).length > 0) {
-    for (let param in additionalParams) {
-      requestParams[param] = additionalParams[param];
-    }
-  }
-  // Роблю запит
-  const responseData = await axios
-    .get('', {
-      params: requestParams,
-    })
-    .then(response => {
-      return response.data; // повертаю тільки дані з реквесту
-    });
-
+  const responseData = await axios.get('', {
+    params: requestParams,
+  });
   return responseData;
 }
--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import iconSvgWarning from './img/warning.svg';
 import getResponseData from './js/pixabay-api.js';
 import addGalleryElements from './js/render-functions.js';
 
-// Оголошую константи, потрібні для роботи
 const galleryList = document.querySelector('.gallery');
 const loaderElement = document.querySelector('.loader');
 const requestForm = document.querySelector('.search-form');
@@ -14,11 +13,10 @@ const loadMoreButton = document.getElementById('loadMoreBtn');
 const goToTopButton = document.querySelector('.go-top-button');
 const searchField = document.querySelector('.search-field');
 
-let page = 1; // Номер сторінки для пагінації
-let responsePhrase = ''; // У цю змінну записую пошукову фразу для передачі у запит на бек
+let page = 1;
+let responsePhrase = '';
 const per_page = 15;
 
-// Параметри повідомлень, які будуть виводитися iziToast
 const errFindImagesMessage = {
   message:
     'Sorry, there are no images matching <br> your search query. Please, try again!',
@@ -47,7 +45,6 @@ const allImagesLoadded = {
   displayMode: 'once',
 };
 
-// Створюю об'єкт галереї
 let gallery = new SimpleLightbox('.gallery a', {
   captions: true,
   captionType: 'attr',
@@ -62,18 +59,12 @@ gallery.on('error.simplelightbox', function (e) {
   console.log(e);
 });
 
-// Додаю лістенери до елементів
-
-// Лістенер для перевірки максимальної довжини введеної пошукової фрази
 requestForm.addEventListener('input', checkMaxLengthRequestWords);
 
-// Лістенер на сабміт форми
 requestForm.addEventListener('submit', searchImages);
 
-// Лістенер завантаження наступної сторінки результатів (пагінація)
 loadMoreButton.addEventListener('click', loadMoreImages);
 
-// Функція перевірки максимальної довжини введеної фрази
 function checkMaxLengthRequestWords(event) {
   if (event.target.value.trim().length > 100) {
     iziToast.show(owerMaxLengthInputMessg);
@@ -81,98 +72,39 @@ function checkMaxLengthRequestWords(event) {
   }
 }
 
-// Функція отримання результатів при сабміті пошукової форми
 function searchImages(event) {
   event.preventDefault();
 
-  // Якщо поле пусте, то не відсилаємо запит
   if (event.currentTarget.requestField.value.trim().length === 0) {
     return;
   }
 
-  // Показуюлоадер - сердечко <3
   loaderElement.classList.remove('visually-hidden');
-  // Очищую галерею
   galleryList.innerHTML = '';
-  // Очищую пошукову фразу
   responsePhrase = '';
-  // Встановлюю номерсторінки, яка має бути по замовчуванні,
-  // передаю її тільки при пагінації
   page = 1;
-  // Приховую кнопку Зававнтажити ше
   loadMoreButton.classList.add('visually-hidden');
 
-  // Читаю пошукову фразу
   const questionPhrase = event.currentTarget.requestField.value.trim();
-  // Запам'ятовую її глобально, для передачі у функцію пагінації
   responsePhrase = questionPhrase;
   requestForm.reset();
 
-  // Роблю запит
   getResponseData(questionPhrase, { per_page })
     .then(data => {
-      // Якщо масив даних порожній, то виводжу повідомлення і ретурнюся
       if (data.hits.length === 0) {
         iziToast.show(errFindImagesMessage);
         return;
       }
-      // Створюю елементи галереї
       addGalleryElements(galleryList, data);
       gallery.refresh();
-      //Ховаю кнопку
       loadMoreButton.classList.remove('visually-hidden');
-      // Якщо кількість елементів у галереї рівна кількості максхітсів у запиті,
-      // то ховаю кнопку і виводжу повідомлення.
-      // Потрібно, якщо на ппершій сторінці менше 15 зображень
 
       //if (data.totalHits === galleryList.childNodes.length) {
       if (Math.ceil(data.totalHits / per_page) === page) {
         loadMoreButton.classList.add('visually-hidden');
         iziToast.show(allImagesLoadded);
       }
-      page++; // Збільшую лічильник торінок
-    })
-    .catch(() => {
-      iziToast.show(errFindImagesMessage);
-    })
-    .finally(() => {
-      loaderElement.classList.add('visually-hidden'); // Ховаю лоадер
-    });
-}
-
-// Функція пагінації - натискання на кнопку завантажити більше
-function loadMoreImages() {
-  // Показую лоадер
-  loaderElement.classList.remove('visually-hidden');
-  // Скролю, щоб лоадер було видно
-  window.scroll({
-    top: document.body.scrollHeight,
-    behavior: 'smooth',
-  });
-  // Відсилаю запит Аксіосом, передаю номер сторінки page як параметр, який буде доадний у параметри запиту
-  getResponseData(responsePhrase, { per_page, page })
-    .then(data => {
-      // Як і при сабміті при нуловій довжині данх виводжу повідомлення і ретьорн
-      if (data.hits.length === 0) {
-        iziToast.show(errFindImagesMessage);
-        return;
-      }
-      // Додаю нові елементи галереї і оновлюю лайтбокс
-      addGalleryElements(galleryList, data);
-      gallery.refresh();
-
-      // Якщо кількість елементів у галереї рівна кількості максхітсів у запиті,
-      // то ховаю кнопку і виводжу повідомлення
-      if (Math.ceil(data.totalHits / per_page) === page) {
-        loadMoreButton.classList.add('visually-hidden');
-        iziToast.show(allImagesLoadded);
-      }
-      // Беру елемент галереї, визначаю висоту і скролю вниз на дві висоти елемента
-      const elementFormeasurement = document.querySelector('.gallery-item');
-      const elementGeometry = elementFormeasurement.getBoundingClientRect();
-      const scrollLongitude = elementGeometry.height * 2;
-      window.scrollBy(0, scrollLongitude);
-      page++; // Збільшую лічильник сторінки
+      page++;
     })
     .catch(() => {
       iziToast.show(errFindImagesMessage);
@@ -182,7 +114,39 @@ function loadMoreImages() {
     });
 }
 
-// Показую кнопку Піднятися на початок, якщо на сторінці є скролл
+function loadMoreImages() {
+  loaderElement.classList.remove('visually-hidden');
+  window.scroll({
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  });
+  getResponseData(responsePhrase, { per_page, page })
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.show(errFindImagesMessage);
+        return;
+      }
+      addGalleryElements(galleryList, data);
+      gallery.refresh();
+
+      if (Math.ceil(data.totalHits / per_page) === page) {
+        loadMoreButton.classList.add('visually-hidden');
+        iziToast.show(allImagesLoadded);
+      }
+      const elementFormeasurement = document.querySelector('.gallery-item');
+      const elementGeometry = elementFormeasurement.getBoundingClientRect();
+      const scrollLongitude = elementGeometry.height * 2;
+      window.scrollBy(0, scrollLongitude);
+      page++;
+    })
+    .catch(() => {
+      iziToast.show(errFindImagesMessage);
+    })
+    .finally(() => {
+      loaderElement.classList.add('visually-hidden');
+    });
+}
+
 window.addEventListener('scroll', () => {
   if (window.scrollY > 0) {
     goToTopButton.classList.remove('visually-hidden');
@@ -191,7 +155,6 @@ window.addEventListener('scroll', () => {
   }
 });
 
-// Обробка натискання на кнопку Піднятися вгору і фокус на полі введення запиту
 goToTopButton.addEventListener('click', () => {
   window.scrollTo({
     top: 0,
